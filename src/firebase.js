c241ac0db43847bd63bfcb28d8f756bf23a74261
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-06-05 05:15:40 +0600

    env file has been added

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASbmuC-mbEUN395EFGBOYez6jY5lwzfLQ",
-  authDomain: "fir-authentication-b0098.firebaseapp.com",
-  projectId: "fir-authentication-b0098",
-  storageBucket: "fir-authentication-b0098.appspot.com",
-  messagingSenderId: "734967910447",
-  appId: "1:734967910447:web:73a94121ee41bfcdde76da",
-  measurementId: "G-YL682M8NGT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
